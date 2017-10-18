8c4b475bb3d80a482e3abdfb7a9cea685c937210
Aaron Ross <aaron@Aarons-MacBook-Pro.local>
2017-10-18 17:11:05 -0400

    Updated ip.sh
    will now check for `ip` and fallback to `ifconfig` if not present

--- ip.sh
+++ ip.sh
@@ -4,7 +4,11 @@ base="$(dirname "$0")"
 source "$base/colors.sh"
 
 wan="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-lan="$(ip a | grep -Eo 'inet (addr:)?([0-9]+\.){3}([0-9]+)' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+if ( command -v ip ); then
+  lan="$(ip a | grep -Eo 'inet (addr:)?([0-9]+\.){3}([0-9]+)' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+else
+  lan="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]+\.){3}([0-9]+)' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+fi
 lanconcat=$(echo $lan | sed 's/\n/ /g')
 
 echo -e "WAN IP: ${blue}$wan${end}"
