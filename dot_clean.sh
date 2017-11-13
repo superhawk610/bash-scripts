9d49c9c217f928a23a6c05766433a1bf8769f0b2
Aaron Ross <superhawk610@gmail.com>
2017-11-12 21:46:21 -0500

    move config to vars.sh

--- dot_clean.sh
+++ dot_clean.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+## Make sure to set $home_dir either in this script
+## or in a sourced script, as below:
+start=$(pwd)
+base="$(dirname "$0")"
+source "$base/vars.sh" 
+
 if [ "$DOT_CLEAN_HAS_VIEWED" == "" ]; then
   export DOT_CLEAN_HAS_VIEWED=1;
-  find /home/server/ucc-local -name '.DS_Store';
-  find /home/server/ucc-local -name '*._*';
+  find "$home_dir" -name '.DS_Store';
+  find "$home_dir" -name '*._*';
   echo "Make sure to run with the following syntax:";
   echo "  . dot_clean.sh";
   echo "Run again to delete.";
 else
   export DOT_CLEAN_HAS_VIEWED="";
-  find /home/server/ucc-local -name '.DS_Store' -delete;
-  find /home/server/ucc-local -name ='*._*' -delete;
+  find "$home_dir" -name '.DS_Store' -delete;
+  find "$home_dir" -name ='*._*' -delete;
   echo "Files cleaned successfully!";
 fi
